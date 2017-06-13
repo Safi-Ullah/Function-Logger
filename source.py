64ac2e379257b12048b5aff466c6c49a8bb3734b
Safi.Ullah <safi.ullah@arbisoft.com>
2017-06-13 17:04:29 +0500

    pep8 followed

--- source.py
+++ source.py
@@ -1,38 +1,40 @@
 import sys
 
+
 def logged(func):
     def func_wrapper(*args, **kwargs):
-        if(len(args)!=0):
+        if len(args) != 0:
             # Printing function name and arguments
-            sys.stdout.write("You called "
-                             + func.__name__
-                             + "("
-                             + str(args[0]))
+            sys.stdout.write("You called " +
+                             func.__name__ +
+                             "(" + str(args[0]))
             # Traversing over the args to print them
             # --------------------------------------
-            for i in range(1,len(args)):
+            for i in range(1, len(args)):
                 sys.stdout.write(", " + str(args[i]))
-            print (")")
+            print(")")
             sys.stdout.flush()
             # --------------------------------------
         else:
             # Printing the name of the function to be logged
-            print ("You called " + func.__name__ + "()")
+            print("You called " + func.__name__ + "()")
 
         # Printing named args
         if len(kwargs) > 0:
-            print ("Named arguments and their values:")
+            print("Named arguments and their values:")
             # Printing kwargs
             for key, value in kwargs.iteritems():
-                print (key, value)
+                print(key, value)
         else:
-            print ("No named arguments passed")
+            print("No named arguments passed")
 
+        return_val = func(*args, **kwargs)
         # Printing the return value by the func
-        print ("It returned " + str(func(*args, **kwargs)))
+        print("It returned " + str(return_val))
 
-        return func(*args, **kwargs)
+        return return_val
     return func_wrapper
+
 
 @logged
 def foo(*args, **kargs):
